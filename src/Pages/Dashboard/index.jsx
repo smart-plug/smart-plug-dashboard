f83ab5cf5c1c6846ee10aa69aa7b692eadcf0e61
Jao <joaovictordecastroa@gmail.com>
2023-05-09 20:50:45 -0300

    Subindo o botão funcionando

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Menu from '../../Layouts/Menu';
 import './index.css';
 import Main from '../../Layouts/Main';
 
 const Dashbboard: React.FC = () => {
+  const [option, setOption] = useState(0);
+
   return (
     <div className="Dashboard">
-      <Menu />
+      <Menu option={option} setOption={setOption} />
       <Main />
     </div>
   );
